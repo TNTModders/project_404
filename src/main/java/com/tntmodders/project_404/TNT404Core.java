1e5e0589c4cb9143b5cd90528f16cc05eb106b02
TomKate <tomo23katakata@gmail.com>
2018-04-21 12:30:58 +0900

    ランダム破壊システム実装

--- src/main/java/com/tntmodders/project_404/TNT404Core.java
+++ src/main/java/com/tntmodders/project_404/TNT404Core.java
@@ -4,17 +4,12 @@ import com.tntmodders.project_404.block.BlockScranton;
 import com.tntmodders.project_404.core.TNT404ModInfoCore;
 import com.tntmodders.project_404.tile.TileEntityScranton;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.gen.feature.WorldGenMinable;
@@ -23,7 +18,7 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -38,6 +33,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
@@ -64,46 +61,63 @@ public class TNT404Core {
 
     @SubscribeEvent
     public void event(LivingEvent.LivingUpdateEvent event) {
-        if (event.getEntityLiving() instanceof EntityPlayer) {
+        if (event.getEntityLiving() instanceof EntityPlayer &&
+                event.getEntityLiving().world.playerEntities.size() <= 1) {
             if (event.getEntityLiving().getDistanceSq(event.getEntityLiving().world.getSpawnPoint()) > 256 &&
                     event.getEntityLiving().world.loadedTileEntityList.stream().noneMatch(
                             tileEntity -> tileEntity instanceof TileEntityScranton &&
                                     event.getEntityLiving().getDistanceSq(tileEntity.getPos()) < 256)) {
-                for (int i = 0; i < 10; i++) {
+                List<String> blocks = new ArrayList<>();
+                event.getEntityLiving().getEntityData().getKeySet().forEach(s -> {
+                    if (s.startsWith("404_")) {
+                        Block block = Block.getBlockFromName(s.replace("404_", ""));
+                        if (block != null) {
+                            blocks.add(block.getRegistryName().toString());
+                        }
+                    }
+                });
+                //blocks.add(Blocks.GRASS);
+                for (int i = 0; i < 50; i++) {
                     Random rand = new Random();
-                    rand.setSeed(event.getEntityLiving().world.getTotalWorldTime());
+                    rand.setSeed(Math.abs(System.currentTimeMillis() / 100));
                     int x = MathHelper.getInt(rand, -7, 8);
                     int y = MathHelper.getInt(rand, -7, 8);
                     int z = MathHelper.getInt(rand, -7, 8);
-                    if (event.getEntityLiving().world.getBlockState(
-                            event.getEntityLiving().getPosition().add(x, y, z)).getBlock() == Blocks.DIRT) {
-                        if (FMLCommonHandler.instance().getSide().isClient() &&
-                                event.getEntityLiving().world.isRemote) {
-                            this.clientEvent(event, x, y, z);
-                        }
+                    Block block = event.getEntityLiving().world.getBlockState(
+                            event.getEntityLiving().getPosition().add(x, y, z)).getBlock();
+                    if (blocks.contains(block.getRegistryName().toString())) {
+                        LOGGER.info("dest:" + block.getLocalizedName());
+
                         if (!event.getEntityLiving().world.isRemote) {
-                            event.getEntityLiving().world.setBlockToAir(
-                                    event.getEntityLiving().getPosition().add(x, y, z));
+                            event.getEntityLiving().world.setBlockState(
+                                    event.getEntityLiving().getPosition().add(x, y, z), Blocks.AIR.getDefaultState());
                         }
                     }
                 }
             }
+
+            if (event.getEntityLiving().world.getWorldTime() % 6000 == 0) {
+                for (int i = 0; i < 5; i++) {
+                    Random rand = new Random();
+                    rand.setSeed(Math.abs(System.currentTimeMillis() / 1000));
+                    Block block = Block.REGISTRY.getRandomObject(rand);
+                    event.getEntityLiving().getEntityData().setBoolean("404_" + block.getRegistryName().toString(),
+                            true);
+                    LOGGER.info(block);
+                }
+                event.getEntityLiving().getEntityData().setBoolean("404_" + event.getEntityLiving().world.getBlockState(
+                        event.getEntityLiving().getPosition().down()).getBlock().getRegistryName().toString(), true);
+            }
         }
     }
 
-    @SideOnly(Side.CLIENT)
-    private void clientEvent(LivingEvent.LivingUpdateEvent event, int x, int y, int z) {
-        IBlockState state =
-                event.getEntityLiving().world.getBlockState(event.getEntityLiving().getPosition().add(x, y, z));
-        SoundType type = state.getBlock().getSoundType(state, event.getEntityLiving().world,
-                event.getEntityLiving().getPosition().add(x, y, z), null);
-        event.getEntityLiving().world.playSound(Minecraft.getMinecraft().player,
-                event.getEntityLiving().getPosition().add(x, y, z), SoundEvents.BLOCK_FIRE_EXTINGUISH,
-                SoundCategory.BLOCKS, 0.5F, 2.6F + (event.getEntityLiving().world.rand.nextFloat() -
-                        event.getEntityLiving().world.rand.nextFloat()) * 0.8F);
-        Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects(
-                event.getEntityLiving().getPosition().add(x, y, z),
-                event.getEntityLiving().world.getBlockState(event.getEntityLiving().getPosition().add(x, y, z)));
+    @SubscribeEvent
+    public void sleep(PlayerSleepInBedEvent event) {
+        event.getEntityPlayer().getEntityData().getKeySet().forEach(s -> {
+            if (s.contains("404_")) {
+                LOGGER.info(s);
+            }
+        });
     }
 
     @SubscribeEvent
